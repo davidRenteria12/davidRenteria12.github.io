38507227c828412eee598e854751ef9b122a0643
David Renteria <renteriia1011@gmail.com>
2025-06-05 00:14:25 -0600

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  site: "https://github.com/davidRenteria12",
+  base: "https://github.com/davidRenteria12/davidRenteria12.github.io",
+});
+
